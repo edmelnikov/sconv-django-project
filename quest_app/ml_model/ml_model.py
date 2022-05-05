ad9ce114abdc0e524be4263844e3ac4a3f756e53
edmelnikov <edmelnikov1@yandex.ru>
2022-05-05 17:24:07 +0300

    added module for the classification model and template for trajectory data

---
+++ quest_app/ml_model/ml_model.py
@@ -0,0 +1,27 @@
+import random
+
+
+# this is a wrapper for the classification model, WIP
+# the input is a dictionary with answers
+# the output is a number of predicted class
+def dummy_model(ans_dict):
+    ans_dict.pop(list(ans_dict.keys())[0], None)  # remove the first element (csrfmiddlewaretoken)
+    ans_dict.pop('15', None)  # pop the answers that don't take part in prediction
+    ans_dict.pop('16', None)
+
+    # data preprocessing
+    ans_list = []
+    for i in range(14):
+        ans_list.append(0)
+    for key in ans_dict.keys():
+        ans_list[int(key) - 1] = int(ans_dict[key])
+
+    # predict using the pretrained model defined elsewhere in this module
+    # the input of the classification model is a list of answers to the questionnaire
+    # the output is the number of the predicted class
+    # class_num = predict(ans_list) or something
+
+    # this is a placeholder
+    #class_num = random.randint(1, 6)  # number of a class
+    class_num = 1
+    return class_num
--- quest_app/views.py
+++ quest_app/views.py
@@ -1,6 +1,8 @@
 from tkinter.messagebox import QUESTION
 from django.shortcuts import render, HttpResponse
-from .models import Question, Answer
+from .models import Question, Answer, Trajectory
+from .ml_model.ml_model import dummy_model
+#import ml_model
 
 # Create your views here.
 
@@ -9,8 +11,12 @@ def index(request):
     questions = Question.objects.order_by('id')
 
     if request.method == "POST":
-        answer1 = list(request.POST.items())
-        print(request.POST.dict())
-        return HttpResponse(answer1[1:])
+
+        print(request.POST)
+        user_answers = request.POST.dict()  # get answers from the form
+        trajectory_num = dummy_model(user_answers)  # predict the trajectory
+        trajectory = Trajectory.objects.get(number=trajectory_num)
+
+        return render(request, 'quest_app/trajectory_descr.html', {'trajectory': trajectory})  # and return the page with the predicted trajectory
     else:
-        return render(request, 'main/index.html', {'questions': questions, 'answers': answers})
+        return render(request, 'quest_app/questionnaire.html', {'questions': questions, 'answers': answers})
