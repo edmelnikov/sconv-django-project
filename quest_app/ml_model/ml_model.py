315e6fb5378c071b3d2cff41866f4167aeb1d0e9
Артем Моисеев <artemterminator@gmail.com>
2022-05-19 13:55:23 +0300

    added model weights and Model class

--- quest_app/ml_model/ml_model.py
+++ quest_app/ml_model/ml_model.py
@@ -1,5 +1,5 @@
 import random
-
+import joblib
 
 # this is a wrapper for the classification model, WIP
 # the input is a dictionary with answers
@@ -25,5 +25,37 @@ def dummy_model(ans_dict):
 
     # this is a placeholder
     #class_num = random.randint(1, 6)  # number of a class
-    class_num = 1
+    class_num = 1   
     return class_num
+
+
+class Model:
+    '''
+    model_path - path to the model, saved by joblib
+    trans_pipe_path - path to the transformations pipeline object, applied to the input data, saved by joblib
+    '''
+
+    def __init__(self, model_path=None, trans_pipe_path=None):
+        if model_path:
+            self._model = joblib.load(model_path)
+
+        if trans_pipe_path:
+            self._trans_pipe = joblib.load(trans_pipe_path)
+
+    def predict(self, X, transform=True):
+        '''
+        This method performs transformations to the input data (if transform=True)
+        and predicts target value
+        X - input data: list(list(int))
+        '''
+        if transform:
+            X = self._trans_pipe.transform(X)
+
+        prediction = self._model.predict(X)
+        return prediction
+
+    def load_model(self, path):
+        self._model = joblib.load(path)
+
+    def load_trans(self, path):
+        self._trans_pipe = joblib.load(path)
