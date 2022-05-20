834e08e461955dd54821bf178f810e636b47580b
katyamalysheva <katyamalysheva2000@yandex.ru>
2022-05-20 18:53:52 +0300

    changed static roots

--- sconv_django_project/settings.py
+++ sconv_django_project/settings.py
@@ -126,9 +126,10 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+#MEDIA_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT = BASE_DIR / 'static'
-
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 
