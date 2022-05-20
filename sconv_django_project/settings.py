69cde5127dfa8fb5f59e2360f7befbcae0741a88
katyamalysheva <katyamalysheva2000@yandex.ru>
2022-05-20 19:08:40 +0300

    commented out staticdirs

--- sconv_django_project/settings.py
+++ sconv_django_project/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     BASE_DIR / 'static',
-]
+]'''
 #MEDIA_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
