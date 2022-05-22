8b4a42cc85fd9c4683e981bcbc83eac5294f748b
askomyagin <sashawqw@mail.ru>
2022-05-22 14:13:23 +0300

    add database

--- sconv_django_project/settings.py
+++ sconv_django_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'sconv_django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_sqlite3.sqlite3',
     }
 }
 
